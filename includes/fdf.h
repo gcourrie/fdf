b7fc03f09fa5f5d5f5c9561385f6721e6176f7c4
Gaspard COURRIER <gcourrie@e2r12p17.42.fr>
2016-03-23 16:19:09 +0100

    hello

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: gcourrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/22 11:47:27 by gcourrie          #+#    #+#             */
-/*   Updated: 2016/03/22 17:23:15 by gcourrie         ###   ########.fr       */
+/*   Updated: 2016/03/23 16:08:30 by gcourrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,18 @@
 # include <fcntl.h>
 #include "../minilibx_macos/mlx.h"
 
-typedef struct	s_size
+typedef struct		s_size
 {
-	int		x;
-	int		y;
-}				t_size;
+	int				x;
+	int				y;
+}					t_size;
 
-int				fdf(int **table, t_size size);
+typedef struct		s_lst
+{
+	char			**str;
+	struct s_lst	*next;
+}					t_lst;
+
+int					fdf(int **table, t_size size);
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -5,139 +5,113 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcourrie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/01/18 17:20:04 by gcourrie          #+#    #+#             */
-/*   Updated: 2016/03/22 17:25:13 by gcourrie         ###   ########.fr       */
+/*   Created: 2016/03/23 13:31:51 by gcourrie          #+#    #+#             */
+/*   Updated: 2016/03/23 16:18:03 by gcourrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/fdf.h"
+#include "fdf.h"
 #include <stdio.h>
 
-static int		*ft_put_str_in_int_tab(t_list *lst, int *size)
+static void	clear_list(t_lst **lst)
 {
-	int			i;
-	int			n;
-	int			*ret;
-	char		*str;
-	int			index;
-
-	i = 0;
-	index = 0;
-	str = (char *)(lst->content);
-	if (!(ret = (int *)malloc(sizeof(int) * (lst->content_size))))
-		return (NULL);
-	while (str[i])
-	{
-		while (str[i] == ' ')
-			i++;
-		n = i;
-		while (str[n] != ' ' && str[n])
-			n++;
-		ret[index++] = ft_atoi(ft_strndup(str, i, n));
-		i = n;
-		if ((int)(lst->content_size) > *size)
-			*size = lst->content_size;
-		while ((int)(lst->content_size) < *size)
-			ret[lst->content_size++] = 0;
-	}
-	return (ret);
-}
-
-static int		**fd_put_in_int_big_table(t_list *lst, int y,
-											int **table, int *size)
-{
-	int			i;
-
-	i = 0;
-	if (!(table = (int **)malloc(sizeof(int *) * y)))
-		return (NULL);
 	while (lst)
 	{
-		table[i] = ft_put_str_in_int_tab(lst, size);
-		lst = lst->next;
-		i++;
+		free((*lst)->str);
+		free(*lst);
+		*lst = (*lst)->next;
 	}
-	return (table);
+	*lst = NULL;
 }
 
-static size_t	ft_count_grp_int(char *str)
+static int	**put_in_int_tab(int **table, t_size size, t_lst *lst, t_lst *begin)
 {
-	size_t		i;
+	int		n;
+	int		i;
 
-	i = 0;
-	while (*str != '\0')
-	{
-		while (*str == ' ')
-			str++;
-		if (*str != ' ' && *str)
-			i++;
-		while (*str != ' ' && *str)
-			str++;
-	}
-	return (i);
-}
-
-static t_list	*ft_map_into_list(int fd, t_list *lst, int *y)
-{
-	char	*str;
-	t_list	*begin;
-
-	get_next_line(fd, &str);
-	lst = ft_lstnew(str, ft_count_grp_int(str));
-	begin = lst;
-	while (get_next_line(fd, &str))
-	{
-		lst->next = ft_lstnew(str, ft_count_grp_int(str));
-		free(str);
-		lst = lst->next;
-		*y += 1;
-	}
-	return (begin);
-}
-
-static void		ft_affichage(int **table, t_list *lst, t_size size)
-{
-	int			i;
-	int			n;
-	int			sizex;
 
 	i = 0;
 	n = 0;
-	sizex = size.x;
-	while (size.y-- > 0)
+	if (!(table = (int **)malloc(sizeof(int *) * size.y)))
+		return (0);
+	lst = begin;
+	while (lst)
 	{
-		sizex = size.x;
-		printf("size.y = %d\n", size.y);
-		printf("size.x = %d\n", size.x);
-		printf("table [%d] = ", i);
-		n = 0;
-		while (sizex-- > 0)
-			printf("%d ", table[i][n++]);
-		i++;
+		if (!(table[i] = (int *)ft_memalloc(sizeof(int) * size.x)))
+			return (0);
+		while (lst->str[n])
+		{
+			table[i][n] = ft_atoi(lst->str[n]);
+			n++;
+		}
 		lst = lst->next;
-		printf("\n");
+		i++;
 	}
+	clear_list(&begin);
+	return (table);
+}
+
+static void     ft_affichage(int **table, int x, int y)
+{
+    int         i;
+    int         n;
+
+    i = 0;
+    n = 0;
+    while (i < y)
+    {
+        n = 0;
+        while (n < x)
+        {
+            printf("%d ", table[i][n]);
+            n++;
+        }
+        i++;
+        printf("\n");
+
+    }
+}
+
+static int		ft_count_str(char **str)
+{
+	int		i;
+
+	i = 0;
+	while (str++)
+		i++;
+	return (i);
 }
 
 int				main(int ac, char **av)
 {
-	t_list	*lst;
-	int		fd;
 	int		**table;
+	t_lst	*begin;
+	t_lst	*lst;
 	t_size	size;
+	int		fd;
+	char	*tmp;
 
+	lst = NULL;
+	table = NULL;
+	printf("A");
 	if (ac == 2)
 	{
-		size.x = 0;
-		size.y = 0;
-		table = NULL;
-		lst = NULL;
+		printf("A");
 		if ((fd = open(av[1], O_RDONLY)) < 0)
-			return (-1);
-		lst = ft_map_into_list(fd, lst, &(size.y));
-		table = fd_put_in_int_big_table(lst, size.y, table, &(size.x));
-		ft_affichage(table, lst, size);
-//		fdf(table, size);
+			return (0);
+		while (get_next_line(fd, &tmp) > 0)
+		{
+			if (!(lst = (t_lst *)malloc(sizeof(t_lst))))
+				return (0);
+			if (ac++ == 2)
+				begin = lst;
+			lst->str = ft_strsplit(tmp, ' ');
+			if (ft_count_str(lst->str) > size.y)
+				size.y = ft_count_str(lst->str);
+			lst = lst->next;
+		}
+		table = put_in_int_tab(table, size, lst, begin);
+		ft_affichage(table, size.x, size.y);
 	}
 	return (0);
 }
